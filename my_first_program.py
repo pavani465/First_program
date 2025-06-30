490bac7a5366fba4870a855742bcc05c8dc5edd7
Pavani Kakarala <pavanikakarala09@gmail.com>
2025-06-30 19:03:23 +0100

    my code is added

---
+++ my_first_program.py
@@ -0,0 +1,10 @@
+name = input("Enter the name: ")
+print("My name is : ",name)
+
+age = int(input("Enter a user age :"))
+print(age)
+
+if age >=18:
+    print("You are eligible for vote!")
+else:
+    print("You are not eligible for vote!")
