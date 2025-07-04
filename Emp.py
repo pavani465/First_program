b4a5bc22219bf7ed15e2cd9ac3f8f14d5ae0d43f
Pavani Kakarala <pavanikakarala09@gmail.com>
2025-07-04 11:05:22 +0100

    new code added emp.py

---
+++ Emp.py
@@ -0,0 +1,7 @@
+name = input("Enter the user name:")
+Emp_id = int(input("Enter the Emp_id:"))
+Emp_id_salary = float(input("Enter the salary:"))
+
+print("User name:",name)
+print("Emp_id",Emp_id)
+print("Emp_salary",Emp_id_salary)
